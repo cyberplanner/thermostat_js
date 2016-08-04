028914550154eb9533c36b8a0864b6e4960921eb
Luke Cartledge <luke@lukecartledge.com>
2016-08-04 13:03:44 +0100

    Ch7 - adds more JQuery and colours the temperature, adds main.css file

--- src/interface.js
+++ src/interface.js
@@ -1,1 +1,32 @@
-'use strict';
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+  function updateTemperature() {
+    $("#temperature").text(thermostat.getCurrentTemperature());
+    $("#temperature, #degrees").attr('class', thermostat.getDisplayColor());
+  };
+  $("#up").click(function(){
+      thermostat.up();
+      updateTemperature();
+  });
+
+  $("#down").click(function(){
+      thermostat.down();
+      updateTemperature();
+  });
+
+  $("#reset").click(function() {
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  $("#power-save-on").click(function() {
+    thermostat.powerSavingOn();
+    $("#power-saving-mode").text("On");
+  });
+
+  $("#power-save-off").click(function() {
+    thermostat.powerSavingOff();
+    $("#power-saving-mode").text("Off");
+  });
+});
