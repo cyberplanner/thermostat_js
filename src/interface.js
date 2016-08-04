6973905bf2bc13ba33cbf916be524a238636515d
Luke Cartledge <luke@lukecartledge.com>
2016-08-04 15:38:14 +0100

    adds open weather map api

--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
+
   function updateTemperature() {
     $("#temperature").text(thermostat.getCurrentTemperature());
     $("#temperature, #degrees").attr('class', thermostat.getDisplayColor());
-  };
+  }
+
   $("#up").click(function(){
       thermostat.up();
       updateTemperature();
@@ -29,4 +31,20 @@ $(document).ready(function() {
     thermostat.powerSavingOff();
     $("#power-saving-mode").text("Off");
   });
+
+  $("#select-city").submit(function(event) {
+    event.preventDefault();
+    var city = $("#current-city").val();
+    displayWeather(city);
+  });
+
+  function displayWeather(city) {
+    var link = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var key = '&appid=2b53a6756fdc4c64828e22d00f507709';
+    var units = '&units=metric';
+    $.get(link + key + units, function(data) {
+      $("#local-temp").text(data.main.temp);
+    });
+  }
+
 });
